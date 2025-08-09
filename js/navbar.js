d6d5ab464940fa6a0ae9e2e9810979d2262d5ef0
chenwumm <496163862@qq.com>
2025-08-09 10:18:33 +0800

    2025年08月09日的提交。

--- js/navbar.js
+++ js/navbar.js
@@ -2,7 +2,7 @@ const navContainer = document.getElementById('nav');
 if (navContainer) {
     const navHTML = `
 <nav><pre>
-<a href="/">主页</a> <a href="/nav/contact.html">联系</a> <a href="/nav/about.html">关于</a> <a href="/nav/ann-his.html">公告历史</a> <a href="/nav/fl.html">友链</a> <a href="/nav/addfl.html">添加友链</a> <a href="http://chenwu.miniban.cn/">社区</a> <a href="/scratch">猫舍</a> <a href="/nav/chat.html">聊天室</a> <a href="/nav/coll.html">收藏</a>
+<a href="/">主页</a><a href="https://chenwuszx.github.io/">分站</a> <a href="/nav/contact.html">联系</a> <a href="/nav/about.html">关于</a> <a href="/nav/ann-his.html">公告历史</a> <a href="/nav/fl.html">友链</a> <a href="/nav/addfl.html">添加友链</a> <a href="http://chenwu.miniban.cn/">社区</a> <a href="/scratch">猫舍</a> <a href="/nav/chat.html">聊天室</a> <a href="/nav/coll.html">收藏</a>
 </pre></nav>
     `;
     navContainer.innerHTML = navHTML;
