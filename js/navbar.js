e167d511b4b9f76de1b26db0074c3d687dd492cc
chenwumm <496163862@qq.com>
2026-02-13 18:56:57 +0800

    2026年02月13日的提交。

--- js/navbar.js
+++ js/navbar.js
@@ -2,7 +2,7 @@ const navContainer = document.getElementById('nav');
 if (navContainer) {
     const navHTML = `
 <nav><pre>
-<a href="/">主页</a> <a href="https://chenwuszx.github.io/">分站</a> <a href="/nav/contact.html">联系</a> <a href="/nav/about.html">关于</a> <a href="/nav/ann-his.html">公告历史</a> <a href="/arch">往年归档</a> <a href="/nav/fl.html">友链</a> <a href="/nav/addfl.html">添加友链</a> <a href="http://chenwu.miniban.cn/">社区</a> <a href="/nav/chat.html">聊天室</a> <a href="/nav/coll.html">收藏</a> <a href="/nav/gopher.html">gopher</a>
+<a href="/">主页</a> <a href="https://chenwuszx.github.io/">分站</a> <a href="/nav/contact.html">联系</a> <a href="/nav/about.html">关于</a> <a href="/nav/ann-his.html">公告历史</a> <a href="/arch">往年归档</a> <a href="/nav/fl.html">友链</a> <a href="/nav/addfl.html">添加友链</a> <a href="http://chenwu.miniban.cn/">社区</a> <a href="/nav/chat.html">聊天室</a> <a href="/nav/coll.html">收藏</a> <a href="/nav/gopher.html">gopher</a> <a href="/nav/gemini.html">gemini</a> <a href="https://chenwu.envs.net/twtxt/">twtxt</a>
 </pre></nav>
     `;
     navContainer.innerHTML = navHTML;
